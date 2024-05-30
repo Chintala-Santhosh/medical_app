59aed9cc81f0035f7f32c9273dc2bf306b0b5160
santhoshchintala <chintala@isthara.com>
2024-05-31 03:24:46 +0530

    Intentional error

--- lib/modules/profile/profile_screen.dart
+++ lib/modules/profile/profile_screen.dart
@@ -17,7 +17,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
     return Scaffold(
       body: Column(
         children:    [
-          const AppBarScreen(title: "PROFILEeeeeeeeeeeeeeeeee",isFromHome:true),
+          const AppBarScreen(title: "PROFILEeeeeeeeeeeeeeeeeee",isFromHome:true),
           BodyScreen(name:"007EDIT,001",date: "01/06/2023(Fri)",isShow: true,),
         ],
       ),
